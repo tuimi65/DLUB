0ac94c16b36a7d717fb25bc047ddd07fedec347f
Code-xed <ngudulu@gmail.com>
2022-07-29 01:59:42 +0530

    new 1

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ class Config:
     def __init__(self):
         self.API_ID = int(os.environ.get("API_ID"))
         self.API_HASH = os.environ.get("API_HASH")
-        self.BOT_TOKEN = "" or os.environ.get("BOT_TOKEN")
-        self.SESSION_STRING = "" or  os.environ.get("SESSION_STRING")
+        self.SESSION_STRING = os.environ.get("SESSION_STRING")
 
 Bot = Config()
